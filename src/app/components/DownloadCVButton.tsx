64e98b032f9b3216a527ed52fa2d86379a6414cc
hienhhcc <thachdau16t@gmail.com>
2025-09-21 11:10:58 +0700

    Update download CV button

--- src/app/components/DownloadCVButton.tsx
+++ src/app/components/DownloadCVButton.tsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 
 export default function DownloadCVButton() {
   return (
-    <Button asChild variant="default" size="sm">
+    <Button
+      asChild
+      variant="default"
+      size="sm"
+      className="relative before:content-[''] before:absolute before:top-0 before:w-full before:h-full before:bg-[linear-gradient(120deg,transparent,rgba(255,255,255,0.4),transparent)] before:animate-shine"
+    >
       <Link href={"/cv.pdf"} download>
         Download CV
       </Link>
