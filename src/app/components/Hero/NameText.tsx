57cf2da7599e18f6d6f427d541036ae5c4028204
hienhhcc <thachdau16t@gmail.com>
2025-09-24 15:29:15 +0700

    feat: Make my name more fancy

--- src/app/components/Hero/NameText.tsx
+++ src/app/components/Hero/NameText.tsx
@@ -1,5 +1,12 @@
 "use client";
 
 export default function NameText() {
-  return <h1 className="text-4xl font-bold">Hi, I&apos;m Vũ Vinh Hiển</h1>;
+  return (
+    <h1 className="text-4xl font-bold">
+      Hi, I&apos;m{" "}
+      <span className="bg-gradient-to-r from-orange-600 to-pink-600 bg-clip-text text-transparent">
+        Vũ Vinh Hiển
+      </span>
+    </h1>
+  );
 }
