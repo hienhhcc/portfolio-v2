a28013dbec121af675fe1fdc5c5be8b77fcd5b69
hienhhcc <thachdau16t@gmail.com>
2025-09-24 15:13:25 +0700

    feat(footer): add all rights reserved

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="border-t py-6 text-center text-sm text-muted-foreground mt-auto">
-      © 2025 Hienhhcc.
+      © 2025 Hienhhcc. All rights reserved
     </footer>
   );
 }
